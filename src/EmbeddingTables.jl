15e78828939b1a0f7c99f98e2e8ecdc779c29c2b
Mark Hildebrand <mhildebrand@ucdavis.edu>
2020-04-13 10:29:57 -0700

    Files generated by PkgTemplates

---
+++ src/EmbeddingTables.jl
@@ -0,0 +1,5 @@
+module EmbeddingTables
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using EmbeddingTables
+using Test
+
+@testset "EmbeddingTables.jl" begin
+    # Write your own tests here.
+end
